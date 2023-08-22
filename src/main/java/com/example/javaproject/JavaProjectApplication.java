60370e769affb16aabbeb3e39a19bf595101658d
betrand <132092468+Betrand1999@users.noreply.github.com>
2023-08-22 15:07:36 -0400

    Update JavaProjectApplication.java

--- src/main/java/com/example/javaproject/JavaProjectApplication.java
+++ src/main/java/com/example/javaproject/JavaProjectApplication.java
@@ -2,12 +2,19 @@ package com.example.javaproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JavaProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaProjectApplication.class, args);
-	}
+   @GetMapping("/")
+   public String hello() {
+       return "<h1>Hello, world!</h1>";
+   }
+   public static void main(String[] args) {
+      SpringApplication.run(JavaProjectApplication.class, args);
+   }
 
 }
